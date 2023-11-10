5b919634a0c7071d57812a77d6122befe14a182c
Clayton Pereira <TAE11657@AE.LOCAL>
2023-11-10 18:02:33 -0300

    Project Created

---
+++ src/test/java/com/claytonpereira/springproject01/Springproject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claytonpereira.springproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
